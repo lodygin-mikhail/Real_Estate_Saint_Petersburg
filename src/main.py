a1f32853f59d6ae079a765a39b3e715f5560b667
Mikhail <lodygin-mikhail@mail.ru>
2025-09-04 00:13:38 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,49 +1,65 @@
+from enum import Enum
+
 import joblib
 import pandas as pd
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from pydantic.v1 import PositiveInt, PositiveFloat
 
 app = FastAPI()
 
 model = joblib.load("models/real_estate_pipeline.joblib")
 
+class PropertyTypeEnum(str, Enum):
+    STUDIO = "Студия"
+    ONE_ROOM = "1-комнатная"
+    TWO_ROOMS = "южный"
+    THREE_ROOMS = "восточный"
+    FOUR_ROOMS = "западный"
+    FIVE_ROOMS = ''
+
 class PropertyFeatures(BaseModel):
-    flat_status: bool
+    flat_status: bool = Field(description='Сдаётся ли в данный момент квартира?')
     num_of_rooms: str
-    total_area: float
-    living_area: float
-    kitchen_area: float
-    floor: int
+    total_area: PositiveFloat
+    living_area: PositiveFloat
+    kitchen_area: PositiveFloat
+    floor: PositiveInt
     metro_station: str
-    minutes_to_metro: int
+    minutes_to_metro: PositiveInt
     transfer_type: str
-    house_age: int
+    house_age: PositiveInt
     is_future_building: bool
 
 class ModelPrediction(BaseModel):
     price: float
 
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
 @app.post("/predict")
-async def predict(data: PropertyFeatures):
-     input_data = {
-        'flat_status': [PropertyFeatures.flat_status],
-        'num_of_rooms': [PropertyFeatures.num_of_rooms],
-        'total_area_m2': [PropertyFeatures.total_area],
-        'living_area_m2': [PropertyFeatures.living_area],
-        'kitchen_area_m2': [PropertyFeatures.kitchen_area],
-        'floor': [PropertyFeatures.floor],
-        'metro_station': [PropertyFeatures.metro_station],
-        'minutes_to_metro': [PropertyFeatures.minutes_to_metro],
-        'transfer_type': [PropertyFeatures.transfer_type],
-        'house_age': [PropertyFeatures.house_age],
-        'is_future_building': [PropertyFeatures.is_future_building]
-     }
-    prediction = model.predict(input_data)
-    return ModelPrediction(price=prediction)
+def predict(features: PropertyFeatures):
+    input_data = {
+        'flat_status': [features.flat_status],
+        'num_of_rooms': [features.num_of_rooms],
+        'total_area_m2': [features.total_area],
+        'living_area_m2': [features.living_area],
+        'kitchen_area_m2': [features.kitchen_area],
+        'floor': [features.floor],
+        'metro_station': [features.metro_station],
+        'minutes_to_metro': [features.minutes_to_metro],
+        'transfer_type': [features.transfer_type],
+        'house_age': [features.house_age],
+        'is_future_building': [features.is_future_building]
+    }
+    data = pd.DataFrame(input_data)
+
+    prediction = model.predict(data)
+
+    return {"prediction": ModelPrediction(price=prediction)}
 
 
-print(model.predict(X))
-
-# if __name__ == '__main__':
-#     import uvicorn
-#     uvicorn.run('main:app', reload=True)
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run('main:app', reload=True)
